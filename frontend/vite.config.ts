06b3db35aea8183327fc141a6cdf2c6536b6ea7d
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-05 22:26:23 -0600

    added the mdx plugin via vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tempo } from "tempo-devtools/dist/vite";
 import path from 'node:path';
+import mdx from "@mdx-js/rollup";
+import rehypeHighlight from "rehype-highlight";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tempo(), react()],
+  plugins: [
+    tempo(),
+    mdx({ rehypePlugins: [rehypeHighlight] }),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "./src")
