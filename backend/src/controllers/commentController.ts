8312f474821a9633b13acd7c47492c464a5f75f9
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-10 14:18:12 -0500

    Removed dead code

--- backend/src/controllers/commentController.ts
+++ backend/src/controllers/commentController.ts
@@ -1,10 +1,1 @@
-// postsController.ts
-import { Request, Response } from 'express';
-
-export const getAllPosts = (req: Request, res: Response) => {
-  const posts = [ /* some fake data or database result */ ];
-  res.json(posts); // send the posts back to the user
-};
-
-
-
+// Placeholder — to be implemented
