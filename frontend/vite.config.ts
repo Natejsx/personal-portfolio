28084dc561272d37e99e9c05484ba21d63c07d7a
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-03-10 23:05:37 -0500

    style: added the tailwind plugin for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from 'node:path';
 import mdx from "@mdx-js/rollup";
 import rehypeHighlight from "rehype-highlight";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     { enforce: 'pre', ...mdx({ rehypePlugins: [rehypeHighlight] }) },
     react({ include: /\.(jsx|tsx|mdx)$/ }),
   ],
