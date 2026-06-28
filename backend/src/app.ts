49752bdb0b4e348137cfc699fd25ce4bc2befd39
Nathaniel Paz <54493706+Simpleboi@users.noreply.github.com>
2026-06-28 17:53:19 -0500

    updated the rate limiting

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 
 const subscriberLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 5,
+  max: 100,
   message: { message: "Too many requests, please try again later." },
   standardHeaders: true,
   legacyHeaders: false,
@@ -23,7 +23,7 @@ const subscriberLimiter = rateLimit({
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
+  max: 100,
   message: { message: "Too many login attempts, please try again later." },
   standardHeaders: true,
   legacyHeaders: false,
